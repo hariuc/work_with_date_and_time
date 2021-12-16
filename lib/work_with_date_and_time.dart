ba3fe459c74788c4beb501be4f884d33992370a7
hariuc <hariuc@mail.ru>
2021-12-17 00:10:21 +0200

    Adding new features
    - beginOfYear
    - beginOfQuarter
    - beginOfMonth
    - beginOfDay
    - beginOfHour
    - beginOfHour
    - beginOfMinute
    - endOfYear
    - endOfQuarter
    - endOfDay
    - endOfHour
    - endOfMinute
    - addDay

--- lib/work_with_date_and_time.dart
+++ lib/work_with_date_and_time.dart
@@ -1,7 +1,76 @@
-library work_with_date_and_time;
+class WorkWithDateAndTime {
+  
+  static DateTime beginOfYear({required DateTime dateTime}) =>
+      DateTime(dateTime.year);
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+  static DateTime beginOfQuarter({required DateTime dateTime}) {
+    final month = dateTime.month;
+    final year = dateTime.year;
+    if (month >= 1 && month <= 3) {
+      return DateTime(year, 1, 1);
+    } else if (month >= 4 && month <= 6) {
+      return DateTime(year, 4, 1);
+    } else if (month >= 7 && month <= 9) {
+      return DateTime(year, 7, 1);
+    } else {
+      return DateTime(year, 10, 1);
+    }
+  }
+
+  static DateTime beginOfMonth({required DateTime dateTime}) =>
+      DateTime(dateTime.year, dateTime.month, 1);
+
+  static DateTime beginOfDay({required DateTime dateTime}) =>
+      DateTime(dateTime.year, dateTime.month, dateTime.day);
+
+  static DateTime beginOfHour({required DateTime dateTime}) =>
+      DateTime(dateTime.year, dateTime.month, dateTime.day, dateTime.hour);
+
+  static DateTime beginOfMinute({required DateTime dateTime}) => DateTime(
+      dateTime.year,
+      dateTime.month,
+      dateTime.day,
+      dateTime.hour,
+      dateTime.minute);
+
+  static DateTime endOfYear({required DateTime dateTime}) =>
+      DateTime(dateTime.year, 12, 31, 23, 59, 59, 999);
+
+  static DateTime endOfQuarter({required DateTime dateTime}) {
+    final month = dateTime.month;
+    final year = dateTime.year;
+    if (month >= 1 && month <= 3) {
+      return DateTime(year, 3, 31, 23, 59, 59, 999);
+    } else if (month >= 4 && month <= 6) {
+      return DateTime(year, 6, 30, 23, 59, 59, 999);
+    } else if (month >= 7 && month <= 9) {
+      return DateTime(year, 9, 30, 23, 59, 59, 999);
+    } else {
+      return DateTime(year, 12, 31, 23, 59, 59, 999);
+    }
+  }
+
+  static DateTime endOfDay({required DateTime dateTime}) =>
+      DateTime(dateTime.year, dateTime.month, dateTime.day, 23, 59, 59, 999);
+
+  static DateTime endOfHour({required DateTime dateTime}) => DateTime(
+      dateTime.year, dateTime.month, dateTime.day, dateTime.hour, 59, 59, 999);
+
+  static DateTime endOfMinute({required DateTime dateTime}) => DateTime(
+      dateTime.year,
+      dateTime.month,
+      dateTime.day,
+      dateTime.hour,
+      dateTime.minute,
+      59,
+      999);
+
+  static DateTime addDay(
+      {required DateTime dateTime, required int numberOfDay}) {
+    if (numberOfDay == 0) return dateTime;
+
+    if (numberOfDay > 0) return dateTime.add(Duration(days: numberOfDay));
+
+    return dateTime.subtract(Duration(days: numberOfDay));
+  }
 }
