4065545bb7a53de801343037b88896f1a6522db8
hariuc <hariuc@mail.ru>
2021-12-18 16:31:47 +0200

    Add descriptions of methods

--- lib/work_with_date_and_time.dart
+++ lib/work_with_date_and_time.dart
@@ -1,19 +1,25 @@
 class WorkWithDateAndTime {
+  //Get the beginning of the year from the specified date
   static DateTime beginOfYear({required DateTime dateTime}) =>
       DateTime(dateTime.year);
 
+  //Get the beginning of the quarter from the specified date
   static DateTime beginOfQuarter({required DateTime dateTime}) =>
       DateTime(dateTime.year, (dateTime.month / 3).ceil(), 1);
 
+  //Get the beginning of the month from the specified date
   static DateTime beginOfMonth({required DateTime dateTime}) =>
       DateTime(dateTime.year, dateTime.month, 1);
 
+  //Get the beginning of the day from the specified date
   static DateTime beginOfDay({required DateTime dateTime}) =>
       DateTime(dateTime.year, dateTime.month, dateTime.day);
 
+  //Get the beginning of the hour from the specified date
   static DateTime beginOfHour({required DateTime dateTime}) =>
       DateTime(dateTime.year, dateTime.month, dateTime.day, dateTime.hour);
 
+  //Get the beginning of the minute from the specified date
   static DateTime beginOfMinute({required DateTime dateTime}) => DateTime(
       dateTime.year,
       dateTime.month,
@@ -21,9 +27,11 @@ class WorkWithDateAndTime {
       dateTime.hour,
       dateTime.minute);
 
+  //Get the end of the year from the specified date
   static DateTime endOfYear({required DateTime dateTime}) =>
       DateTime(dateTime.year, 12, 31, 23, 59, 59, 999);
 
+  //Get the end of the quarter from the specified date
   static DateTime endOfQuarter({required DateTime dateTime}) {
     final month = dateTime.month;
     final year = dateTime.year;
@@ -38,6 +46,7 @@ class WorkWithDateAndTime {
     }
   }
 
+  //Get the end of the month from the specified date
   DateTime endOfMonth({required DateTime dateTime}) {
     if (dateTime.month == 2) {
       var day = 28;
@@ -56,12 +65,15 @@ class WorkWithDateAndTime {
     return DateTime(dateTime.year, dateTime.month, 31, 23, 59, 59, 999);
   }
 
+  //Get the end of the day from the specified date
   static DateTime endOfDay({required DateTime dateTime}) =>
       DateTime(dateTime.year, dateTime.month, dateTime.day, 23, 59, 59, 999);
 
+  //Get the end of the hour from the specified date
   static DateTime endOfHour({required DateTime dateTime}) => DateTime(
       dateTime.year, dateTime.month, dateTime.day, dateTime.hour, 59, 59, 999);
 
+  //Get the end of the minute from the specified date
   static DateTime endOfMinute({required DateTime dateTime}) => DateTime(
       dateTime.year,
       dateTime.month,
@@ -71,6 +83,9 @@ class WorkWithDateAndTime {
       59,
       999);
 
+  //Get a new date by adding or subtracting days
+  //If numberOfDay is positive, then the days are added
+  // If numberOfDay is negative, then the days are taken away
   static DateTime addDay(
       {required DateTime dateTime, required int numberOfDay}) {
     if (numberOfDay == 0) return dateTime;
@@ -80,6 +95,7 @@ class WorkWithDateAndTime {
     return dateTime.subtract(Duration(days: numberOfDay));
   }
 
+  //Is it a leap year ?
   static bool isLeapYear({required DateTime dateTime}) {
     if (dateTime.year % 4 == 0) {
       if (dateTime.year % 100 == 0) {
